71db00b03fcd8e2b5fc396b6b8f247c096c47b63
YuLaFreitas <iruytj@gmail.com>
2022-04-06 11:10:23 -0300

    Criado REST_API

---
+++ 0x02/construindo_API_REST/src/main/java/com/song/demo/Song.java
@@ -0,0 +1,51 @@
+package com.song.demo;
+
+public class Song {
+    private Integer id;
+    private String nome;
+    private String artista;
+    private String album;
+    private String anoLancamento;
+    
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArtista() {
+        return this.artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getAnoLancamento() {
+        return this.anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+    
+}
