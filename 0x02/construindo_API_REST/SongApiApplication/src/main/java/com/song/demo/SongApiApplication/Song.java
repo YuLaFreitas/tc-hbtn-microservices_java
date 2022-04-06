da0e11eb06e54b16a62bcfb1c57893bfa289f769
YuLaFreitas <iruytj@gmail.com>
2022-04-06 14:48:10 -0300

    TASK

---
+++ 0x02/construindo_API_REST/SongApiApplication/src/main/java/com/song/demo/SongApiApplication/Song.java
@@ -0,0 +1,62 @@
+package com.song.demo.SongApiApplication;
+
+public class Song {
+    private Integer id;
+    private String nome;
+    private String artista;
+    private String album;
+    private String anoLancamento;
+    
+    
+    @Override
+    public String toString() {
+        return "{" +
+            " id='" + getId() + "'" +
+            ", nome='" + getNome() + "'" +
+            ", artista='" + getArtista() + "'" +
+            ", album='" + getAlbum() + "'" +
+            ", anoLancamento='" + getAnoLancamento() + "'" +
+            "}";
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getArtista() {
+        return this.artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return this.album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getAnoLancamento() {
+        return this.anoLancamento;
+    }
+
+    public void setAnoLancamento(String anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+    
+}
